b1240ef6e1ae65845da154e49658dcfb6bf6d9d0
iivan <iivasheprut@gmail.com>
2025-08-23 13:21:40 +0300

    POSt kuva

--- instagram_clone/stories/urls.py
+++ instagram_clone/stories/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = "stories"
-
 urlpatterns = [
-    path("", views.story_list, name="story_list"),
-    path("add/", views.add_story, name="add_story"),
+    path('', views.story_list, name='story_list'),
+    path('create/', views.story_create, name='story_create'),  # <- вот эта строка!
 ]
