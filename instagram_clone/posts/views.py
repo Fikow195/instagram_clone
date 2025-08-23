09e8c2a636818e9f626c3333542e19cb9128e44d
iivan <iivasheprut@gmail.com>
2025-08-23 12:00:12 +0300

    +

--- instagram_clone/posts/views.py
+++ instagram_clone/posts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostListCreateView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        posts = Post.objects.all().order_by('-created_at')
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
