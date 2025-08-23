4ef677b8c707aafb09d9156accd711def968bb4a
iivan <iivasheprut@gmail.com>
2025-08-23 13:11:35 +0300

    POSt kuva

--- instagram_clone/posts/views.py
+++ instagram_clone/posts/views.py
@@ -1,14 +1,14 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Post
-from .forms import PostForm
+from .models import Post, Comment
+from .forms import PostForm, CommentForm
 
-# список постов
+
 def posts_list(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'posts/posts_list.html', {'posts': posts})
 
-# создание поста
+
 @login_required
 def post_create(request):
     if request.method == 'POST':
@@ -21,3 +21,27 @@ def post_create(request):
     else:
         form = PostForm()
     return render(request, 'posts/post_form.html', {'form': form})
+
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.post = post
+            comment.save()
+            return redirect('posts_list')
+    else:
+        form = CommentForm()
+    return render(request, 'posts/add_comment.html', {'form': form, 'post': post})
+
+@login_required
+def like_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.user in post.likes.all():
+        post.likes.remove(request.user)
+    else:
+        post.likes.add(request.user)
+    return redirect('posts_list')
