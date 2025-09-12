7b6145ba1d9565ce21ed6b16c9c672002a58c85d
Fikow195 <123456timur195@gmail.com>
2025-09-12 21:41:08 +0200

    Самое большое обновление проекта за наверняка всё время его создания. Спасибо богу за то что направил душу мою грешную на этот путь окоянный. Да прибудет сила моя богатырскаяъ да уверуют неверующие. Ave Maria!

--- instagram_clone/chat/storage_backends.py
+++ instagram_clone/chat/storage_backends.py
@@ -1,7 +1,2 @@
 from storages.backends.azure_storage import AzureStorage
 
-class AzureMediaStorage(AzureStorage):
-    account_name = "stoga4"
-    account_key = "bVBakeCt8U4PA4R02dvCULtdUu8ttR/KGYRmgDwMMzxOe4rDrEWqDbsg1UMjwDVY5DljFOIzAbpx+AStD0ISUg=="
-    azure_container = "media"
-    expiration_secs = None
